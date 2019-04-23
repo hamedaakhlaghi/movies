e1eb0e2b0f9c38e107b5ba44f36c1f2e0a749697
Hamed <akhlaghi65@gmail.com>
2019-04-24 00:00:51 +0430

    Initial commit closed #1

---
+++ Movies/ui/main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Hamed on 4/21/19.
+//  Copyright © 2019 Hamed. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
